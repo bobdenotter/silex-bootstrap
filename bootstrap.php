717883ff07f527b4e81510a01f14b5ea5ad0e299
Bob den Otter <bob@twokings.nl>
2012-06-10 10:11:50 +0200

    Removed debug.log from repo

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,16 @@
 <?php
 
+if (!file_exists(__DIR__.'/config.php')) {
+    echo "<p>The file <tt>config.php</tt> doesn't exist. Copy <tt>config.php.dist</tt> to <tt>config.php</tt> and add the correct settings.</p>";
+    die();
+}
+
+if (!file_exists(__DIR__.'/vendor/autoload.php')) {
+    echo "<p>The file <tt>vendor/autoload.php</tt> doesn't exist. Make sure you've installed the Silex components with Composer. See the README.md file.</p>";
+    die();
+}
+
+
 require_once __DIR__.'/config.php';
 require_once __DIR__.'/vendor/autoload.php';
 require_once __DIR__.'/lib.php';
