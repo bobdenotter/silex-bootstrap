440edda840463961d121eb40fa7d9afd2cab9251
Bob den Otter <bob@twokings.nl>
2012-06-14 16:24:25 +0200

    Shuffled order in index.php around..

--- index.php
+++ index.php
@@ -11,8 +11,8 @@ if (!file_exists(__DIR__.'/vendor/autoload.php')) {
 }
 
 require_once __DIR__.'/config.php';
-require_once __DIR__.'/bootstrap.php';
 require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/bootstrap.php';
 require_once __DIR__.'/app.php';
 require_once __DIR__.'/lib.php';
 
